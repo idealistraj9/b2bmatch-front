5e8ac0e82e4b100562423871189ff56db07df0f5
BildKomGit <54430070+BildKomGit@users.noreply.github.com>
2024-03-26 13:27:22 +0100

    pz

--- components/promptRes.tsx
+++ components/promptRes.tsx
@@ -5,7 +5,7 @@ const PromptRes = () => {
       <div className="flex flex-col w-full rounded-lg pt-2 pb-2">
         <div className=" w-full bg-response border-bor border-2 rounded-lg p-3 text-lg max-h-34 overflow-auto ">
           <p className="  text-lg">
-            Hier wird Ihr Suchergebnis verbal angezeigt. Die Graphik zeigt Ihnen das gleiche mit mehr Details.
+            Hier wird Ihr Suchergebnis verbal angezeigt. Die Grafik zeigt Ihnen das gleiche mit mehr Details.
           </p>
           <p className="  text-lg">
             {/* Text field left intentionally empty */}
